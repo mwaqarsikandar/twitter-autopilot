0a7d71055b6349032b5b51e559843f0d05a7de1f
Muhammad Waqar Sikandar <muhammadwaqarfiverr@gmail.com>
2020-06-17 11:28:33 +0500

    updated

--- src/config.js
+++ src/config.js
@@ -7,8 +7,8 @@
 require('dotenv').config();
 
 module.exports = {
-  consumer_key: '',
-  consumer_secret: '',
-  access_token: '',
-  access_token_secret: '',
+  consumer_key: 'kSL2HbW9mZ0xG28zWKex7yWfC',
+  consumer_secret: 'oVFdmsmfMmkg7Gw6UeQRuzb3KoRt2vznTvf9NoM9dPiNbouiZh',
+  access_token: '1237376892170383360-NAHE1MzTOhqAceNanBLPlZnjmaZET2',
+  access_token_secret: 's3qQWbV3iLxL5d7Mz3XIYmo2w2hBzXOssOVKhR0qucnlt',
 };
