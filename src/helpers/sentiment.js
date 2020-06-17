b3d315c1fab3e74efbe855a39233e007e6dc32fa
Muhammad Waqar Sikandar <muhammadwaqarfiverr@gmail.com>
2020-06-17 11:30:14 +0500

    update

--- src/helpers/sentiment.js
+++ src/helpers/sentiment.js
@@ -4,16 +4,19 @@ const unirest = require('unirest')
   Get a new API key at https://market.mashape.com/vivekn/sentiment-3
 */
 
+
+
 // Sentiment 3 Mashape API key
-var apiKey = 'QqtO3XbGhFmshEmKBxy58FqKLvG3p1rx61ijsnaHTstuRd3jp0'
+
 
 var sentiment = {}
 
 sentiment.init = function () {
-  return unirest.post('https://community-sentiment.p.mashape.com/text/')
-  .header('X-Mashape-Key', apiKey)
-  .header('Content-Type', 'application/x-www-form-urlencoded')
-  .header('Accept', 'application/json')
+  return unirest.post('https://twinword-sentiment-analysis.p.rapidapi.com/analyze/')
+  .header('x-rapidapi-host', 'twinword-sentiment-analysis.p.rapidapi.com')
+  .header('x-rapidapi-key', '70fbaac2d3msh4b098fcf0c079e8p192409jsn67a81f80c7e9')
+  .header('content-type', 'application/x-www-form-urlencoded')
+  .header('useQueryString', true)
 }
 
 module.exports = sentiment
